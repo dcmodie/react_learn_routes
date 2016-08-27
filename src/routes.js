fb90f9b5b51538f98e400e9af5a70cc002d602d6
Doug Modie <dmodie@gmail.com>
2016-08-27 15:55:33 -0500

    set up index route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app.js'
+import PostsIndex from './components/posts_index.js'
 
 const Greeting = () => {
 	return <div> hey here</div>
@@ -9,6 +10,7 @@ const Greeting = () => {
 
 export default (
 	<Route path="/" component={App} >
+		<IndexRoute component={PostsIndex} />
 		<Route path="greet" component={Greeting}/>
 		<Route path="greet1" component={Greeting}/>
 		<Route path="greet2" component={Greeting}/>
