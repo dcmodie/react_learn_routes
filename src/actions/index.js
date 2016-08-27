640a0ec6ddb6462b89b9cc382dd33116b794d900
Doug Modie <dmodie@gmail.com>
2016-08-27 18:32:43 -0500

    calling action from component willmount

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,10 @@ const API_KEY = '?key=123lhasdglkjhasdfkljhasfklhswkjh'
 export function fetchPosts(){
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
+
+
+	// make api call
+	console.log ("action called")
 	return{
 		type:FETCH_POSTS,
 		payload:request
