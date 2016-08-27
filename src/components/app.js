f962e9ed5aa217179c271be326621357b35cea76
Doug Modie <dmodie@gmail.com>
2016-08-27 15:40:05 -0500

    nested routes

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React Learn Routes</div>
+      <div>
+      	React Learnasdf Routes
+      	{this.props.children}
+      </div>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app.js'
+
+const Greeting = () => {
+	return <div> hey here</div>
+}
+
 export default (
-	<Route path="/" component={App} />
+	<Route path="/" component={App} >
+		<Route path="greet" component={Greeting}/>
+		<Route path="greet1" component={Greeting}/>
+		<Route path="greet2" component={Greeting}/>
+	</Route>
 )
