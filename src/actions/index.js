05a4449b51b5851987f7b5739bbca0abbcca080a
Doug Modie <dmodie@gmail.com>
2016-08-27 21:29:42 -0500

    sorta getting index back

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,19 @@ export const FETCH_POSTS= 'FETCH_POSTS'
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=123lhasdglkjhasdfkljhasfklhswkjh'
 export function fetchPosts(){
+
+	var url = `${ROOT_URL}/posts/${API_KEY}`
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
-
+	console.log ("url")
+	console.log (url)
 
 	// make api call
 	console.log ("action called")
+	console.log (request)
+	//payload.data
+
+
 	return{
 		type:FETCH_POSTS,
 		payload:request
