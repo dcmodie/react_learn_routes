557c67b1892e92d6f40c0f9caaff28e885fb1b49
Doug Modie <dmodie@gmail.com>
2016-09-01 11:19:28 -0500

    dynamic routing set up

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app.js'
 import PostsNew from './components/posts_new.js'
 import PostsIndex from './components/posts_index.js'
+import PostsShow from './components/posts_show.js'
 
 const Greeting = () => {
 	return <div> hey here</div>
@@ -13,8 +14,11 @@ export default (
 	<Route path="/" component={App} >
 		<IndexRoute component={PostsIndex} />
 		<Route path="posts/new" component={PostsNew}/>
+		<Route path="posts/:id" component={PostsShow}/>
 		<Route path="greet" component={Greeting}/>
 		<Route path="greet1" component={Greeting}/>
 		<Route path="greet2" component={Greeting}/>
 	</Route>
 )
+// that id will be seen as 
+//this.props.params.id
